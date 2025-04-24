2feaed7b1c044a8a6c4d3c35117fbed993610fcf
Christopher.Lundqvist <christopher.lundqvist@ecarxgroup.com>
2025-04-24 12:53:40 +0200

    Initial Spring Boot backend setup

---
+++ backend/src/main/java/example/nba/NbaStatsBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.nba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaStatsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbaStatsBackendApplication.class, args);
+    }
+}
